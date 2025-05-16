b25490a5f5fb96b25d4d01b801b3dd5ed3293e3b
HugoBar <hugomdbarreiro@gmail.com>
2025-05-16 15:35:42 +0100

    Add meow and config adjustments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log("Hello World!")
+import meow from 'meow'
+
+const cli = meow(`
+	Usage
+	  $ foo <input>
+
+	Options
+	  --rainbow, -r  Include a rainbow
+
+	Examples
+	  $ foo unicorns --rainbow
+	  🌈 unicorns 🌈
+`, {
+	importMeta: import.meta,
+	flags: {
+		rainbow: {
+			type: 'boolean',
+			shortFlag: 'r'
+		}
+	}
+});
