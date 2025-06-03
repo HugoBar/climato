e6321761f105de10360ce35ea95d6ca147e3b987
HugoBar <hugomdbarreiro@gmail.com>
2025-06-03 15:51:11 +0100

    Ensure cities.json is available in dist after build

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,14 @@
 import { readFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
 import { City } from "./interfaces/city.js";
 
 export function findOnTheMap(cityName: string): City {
-  const citiesRaw = readFileSync("./src/json/cities.json", "utf8");
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const citiesRaw = readFileSync(join(__dirname, "json/cities.json"), "utf8");
   const cities = JSON.parse(citiesRaw);
 
   return cities.find((c: City) => c.local === cityName);
